7501910940c656079089a25f3edb345279088af1
Mohammed ESSAM <mohammed.essam@valeo.com>
2017-11-22 09:43:19 +0200

    updatng nvidia net by adding dropout layer

--- clone.py
+++ clone.py
@@ -6,10 +6,9 @@ from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Cropping2D, Convolution2D, MaxPooling2D, Dropout
+from keras.layers import Flatten, Dense, Lambda, Cropping2D, Conv2D, MaxPooling2D, Dropout
 
-def generate_data(paths, split_str, flip=False):
-    correction = 0.2  # this is a parameter to tune
+def generate_data(paths, split_str, flip=False, use_side_cameras=False, correction=0.2):
     images = []
     measurements = []
     print('Reading Images')
@@ -25,26 +24,42 @@ def generate_data(paths, split_str, flip=False):
                     lines.append(line)
 
         for line in lines:
-            for i in range(3):
-                source_path = line[i]
-                file_name = source_path.split(split_str)[-1]
-                current_path = path + 'IMG/' + file_name
-                image = cv2.imread(current_path)
-                images.append(image)
-                if i == 0:
+            if use_side_cameras:
+                for i in range(3):
+                    source_path = line[i]
+                    file_name = source_path.split(split_str)[-1]
+                    current_path = path + 'IMG/' + file_name
+                    image = cv2.imread(current_path)
+                    images.append(image)
+                    if i == 0:
+                        measurement = float(line[3])
+                    elif i == 1:
+                        measurement = float(line[3]) + correction
+                    elif i == 2:
+                        measurement = float(line[3]) - correction
+
+                    measurements.append(measurement)
+
+                    if flip:
+                        new_image = cv2.flip(image, 1)
+                        new_measurement = measurement * (-1)
+                        images.append(new_image)
+                        measurements.append(new_measurement)
+                else:
+                    source_path = line[0]
+                    file_name = source_path.split(split_str)[-1]
+                    current_path = path + 'IMG/' + file_name
+                    image = cv2.imread(current_path)
+                    images.append(image)
                     measurement = float(line[3])
-                elif i == 1:
-                    measurement = float(line[3]) + correction
-                elif i == 2:
-                    measurement = float(line[3]) - correction
+                    measurements.append(measurement)
 
-                measurements.append(measurement)
+                    if flip:
+                        new_image = cv2.flip(image, 1)
+                        new_measurement = measurement * (-1)
+                        images.append(new_image)
+                        measurements.append(new_measurement)
 
-                if flip:
-                    new_image = cv2.flip(image, 1)
-                    new_measurement = measurement * (-1)
-                    images.append(new_image)
-                    measurements.append(new_measurement)
         del lines
 
     print("Testing data number:", len(images))
@@ -74,8 +89,7 @@ def generate_samples(paths):
     return samples
 
 
-def generator(samples, split_str, batch_size_=64):
-    correction = 0.2
+def generator(samples, split_str, flip=False,batch_size_=64, use_side_cameras=False, correction=0.2):
     num_samples = len(samples)
     while 1:  # Loop forever so the generator never terminates
         shuffle(samples)
@@ -85,19 +99,29 @@ def generator(samples, split_str, batch_size_=64):
             images = []
             angles = []
             for batch_sample in batch_samples:
-                i = random.randint(0, 2)
-                source_path = batch_sample[i]
-                current_path = '..' + source_path.split(split_str)[-1].replace('\\','/')
-                image = cv2.imread(current_path)
-                if i == 0:
-                    angle = float(batch_sample[3])
-                elif i == 1:
-                    angle = float(batch_sample[3]) + correction
-                elif i == 2:
-                    angle = float(batch_sample[3]) - correction
+                if use_side_cameras:
+                    for i in range(0,3):
+                        source_path = batch_sample[i]
+                        # current_path = '..' + source_path.split(split_str)[-1].replace('\\', '/')
+                        current_path = '../data/' + source_path.split(split_str)[-1]
+                        image = cv2.imread(current_path)
+                        if i == 0:
+                            angle = float(batch_sample[3])
+                        elif i == 1:
+                            angle = float(batch_sample[3]) + correction
+                        elif i == 2:
+                            angle = float(batch_sample[3]) - correction
 
-                images.append(image)
-                angles.append(angle)
+                        images.append(image)
+                        angles.append(angle)
+                else:
+                    source_path = batch_sample[0]
+                    current_path = '..' + source_path.split(split_str)[-1].replace('\\','/')
+                    image = cv2.imread(current_path)
+                    angle = float(batch_sample[3])
+                    images.append(image)
+                    angles.append(angle)
+
 
             # trim image to only see section with road
             X_train = np.array(images)
@@ -117,9 +141,9 @@ def simple_net():
 def le_net():
     model = Sequential()
     model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160, 320, 3)))
-    model.add(Convolution2D(6, 5, 5, activation="relu"))
+    model.add(Conv2D(6, (5, 5), activation="relu"))
     model.add(MaxPooling2D())
-    model.add(Convolution2D(6, 5, 5, activation="relu"))
+    model.add(Conv2D(6, (5, 5), activation="relu"))
     model.add(MaxPooling2D())
     model.add(Flatten())
     model.add(Dense(120))
@@ -131,13 +155,15 @@ def le_net():
 
 def nvidia_net():
     model = Sequential()
-    model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160, 320, 3)))
-    model.add(Convolution2D(24,5,5, subsample=(2,2), activation='relu'))
-    model.add(Convolution2D(36,5,5, subsample=(2,2), activation='relu'))
-    model.add(Convolution2D(48,5,5, subsample=(2,2), activation='relu'))
-    model.add(Convolution2D(64,3,3, activation='relu'))
-    model.add(Convolution2D(64,3,3, activation='relu'))
+    model.add(Cropping2D(cropping=((40, 20), (0, 0)), input_shape=(160, 320, 3)))
+    model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(64, 64, 3)))
+    model.add(Conv2D(24, (5, 5), activation="relu", strides=(2, 2)))
+    model.add(Conv2D(36, (5, 5), activation="relu", strides=(2, 2)))
+    model.add(Conv2D(48, (5, 5), activation="relu", strides=(2, 2)))
+    model.add(Conv2D(64, (3, 3), activation="relu"))
+    model.add(Conv2D(64, (3, 3), activation="relu"))
     model.add(Flatten())
+    model.add(Dropout(0.5))
     model.add(Dense(100))
     model.add(Dense(50))
     model.add(Dense(10))
@@ -146,36 +172,58 @@ def nvidia_net():
 
 
 if __name__ == "__main__":
+    use_generator = True
+    use_side_cameras = True
+    flip = False
+
+    correction = 0.2  # this is a parameter to tune
     epochs = 10
-    batch_size = 512
+    real_batch_size = 32
+    if use_side_cameras:
+        batch_size = int(real_batch_size/3)
+    else:
+        batch_size = real_batch_size
 
     # model, model_name = simple_net()
-    model, model_name = le_net()
-    # model, model_name = nvidia_net()
+    # model, model_name = le_net()
+    model, model_name = nvidia_net()
     model.summary()
 
-    # paths, split_str = ['../data/'], '/'
-    # paths, split_str = ['../track1/2labs_center/', '../track1/1lab_recovery/', '../track1/1lab_smothcurve/', '../track1/2labs_CC/'], '\\'
-    # paths, split_str = ['../track2/2labs_center/', '../track2/1lab_recovery/', '../track2/1lab_smothcurve/', '../track2/2labs_CC/'], '\\'
-    # paths, split_str = ['../track1/2labs_center/', '../track1/1lab_recovery/'], '\\'
-    # X_train, y_train = generate_data(paths=paths, split_str=split_str, flip=False)
+    print("Using generator: ", use_generator)
+    print("Using side cameras: ", use_side_cameras)
+    print("Flipping images: ", flip)
+    print("Using", model_name," model")
+    print("Epochs: ", epochs)
+    print("Batch size: ", real_batch_size)
 
-    paths, split_str = ['../track1/2labs_center/', '../track1/1lab_recovery/'], '07_Behavioral_Cloning'
-    samples = generate_samples(paths)
-    train_samples, validation_samples = train_test_split(samples, test_size=0.2)
+    if use_generator:
+        # paths, split_str = ['../track1/1lab_center/', '../track1/curves_recovery/'] , '07_Behavioral_Cloning'
+        paths, split_str = ['../data/'] , ' '
+        samples = generate_samples(paths)
+        train_samples, validation_samples = train_test_split(samples, test_size=0.2)
 
-    train_generator = generator(train_samples, split_str=split_str, batch_size_=batch_size)
-    validation_generator = generator(validation_samples, split_str=split_str, batch_size_=batch_size)
+        train_generator = generator(train_samples, split_str=split_str, batch_size_=batch_size,
+                                    use_side_cameras=use_side_cameras, correction=correction)
+        validation_generator = generator(validation_samples, split_str=split_str, batch_size_=batch_size,
+                                         use_side_cameras=use_side_cameras, correction=correction)
 
-    model.compile(loss='mse', optimizer='adam')
+        model.compile(loss='mse', optimizer='adam')
 
-    # history_object = model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=epochs, batch_size=batch_size)
-
-    history_object = model.fit_generator(train_generator,
-                                         samples_per_epoch=len(train_samples)/batch_size,
+        history_object = model.fit_generator(train_generator,
+                                         samples_per_epoch=len(train_samples) / batch_size,
                                          validation_data=validation_generator,
-                                         nb_val_samples=len(validation_samples)/batch_size,
+                                         nb_val_samples=len(validation_samples) / batch_size,
                                          epochs=epochs)
+    else:
+        # paths, split_str = ['../data/'], '/'
+        # paths, split_str = ['../track1/2labs_center/', '../track1/1lab_recovery/', '../track1/1lab_smothcurve/', '../track1/2labs_CC/'], '\\'
+        # paths, split_str = ['../track2/2labs_center/', '../track2/1lab_recovery/', '../track2/1lab_smothcurve/', '../track2/2labs_CC/'], '\\'
+        paths, split_str = ['../track1/2labs_center/', '../track1/1lab_recovery/'], '\\'
+        X_train, y_train = generate_data(paths=paths, split_str=split_str, flip=flip, use_side_cameras=use_side_cameras, correction=correction)
+
+        model.compile(loss='mse', optimizer='adam')
+
+        history_object = model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=epochs, batch_size=batch_size)
 
 
     save_str = 'models/' + model_name + '_e' + str(epochs) + '_b' + str(batch_size) + '.h5'
